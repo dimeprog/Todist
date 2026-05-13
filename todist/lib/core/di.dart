8c0a37fc7189e69dd7ceb170f7c99fc90d00c4db
dimeprog <dimejiawoyefa5@gmail.com>
2026-05-13 04:05:54 +0100

    update

--- todist/lib/core/di.dart
+++ todist/lib/core/di.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/foundation.dart';
-import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:todist/keys.dart';
 
-GetIt getIt = GetIt.instance;
+
 
 Future<void> setUp() async {
    await Hive.initFlutter();
